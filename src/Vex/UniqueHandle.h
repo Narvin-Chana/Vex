7296e21b6dcfbcc7ada84bd1f76f4fedcec76b5a
Narvin Chana <narvin.chana@gmail.com>
2025-03-29 21:53:55 -0400

    feat: finished implementing unique handle for bounded array types

--- src/Vex/UniqueHandle.h
+++ src/Vex/UniqueHandle.h
@@ -18,8 +18,22 @@ struct DefaultDeleter
     }
 };
 
-// TODO: doesn't work if T is an array type, potentially not that important.
+template <class T>
+struct DefaultDeleter<T[]>
+{
+    constexpr DefaultDeleter() noexcept = default;
+
+    void operator()(T* ptr) const noexcept
+    {
+        static_assert(sizeof(T) > 0, "Cannot delete an incomplete type.");
+        delete[] ptr;
+    }
+};
+
 template <class T, class Deleter = DefaultDeleter<T>>
+class UniqueHandle;
+
+template <class T, class Deleter>
 class UniqueHandle
 {
 public:
@@ -27,7 +41,6 @@ public:
     constexpr explicit UniqueHandle(T* ptr)
         : ptr(ptr)
     {
-        static_assert(std::is_array_v<T> == false, "UniqueHandle does not currently support arrays.");
     }
 
     ~UniqueHandle()
@@ -53,14 +66,103 @@ public:
         return *this;
     }
 
-    auto Get() -> T*
+    T* operator->() const noexcept
     {
         return ptr;
     }
 
-    auto operator->() -> T&
+    T& operator*() const noexcept
     {
         return *ptr;
+    }
+
+    void Reset(T* newPtr = nullptr) noexcept
+    {
+        Deleter{}(ptr);
+        ptr = newPtr;
+    }
+
+    T* Release() noexcept
+    {
+        return std::exchange(ptr, nullptr);
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return ptr != nullptr;
+    }
+
+    T* Get()
+    {
+        return ptr;
+    }
+
+private:
+    T* ptr = nullptr;
+};
+
+template <class T, class Deleter>
+class UniqueHandle<T[], Deleter>
+{
+public:
+    constexpr UniqueHandle() = default;
+    constexpr explicit UniqueHandle(T* ptr)
+        : ptr(ptr)
+    {
+    }
+
+    ~UniqueHandle()
+    {
+        Deleter{}(ptr);
+    }
+
+    UniqueHandle(const UniqueHandle&) = delete;
+    UniqueHandle& operator=(const UniqueHandle&) = delete;
+
+    UniqueHandle(UniqueHandle&& other) noexcept
+        : ptr(std::exchange(other.ptr, nullptr))
+    {
+    }
+
+    UniqueHandle& operator=(UniqueHandle&& other) noexcept
+    {
+        if (this != &other)
+        {
+            Deleter{}(ptr);
+            ptr = std::exchange(other.ptr, nullptr);
+        }
+        return *this;
+    }
+
+    T* Get()
+    {
+        return ptr;
+    }
+
+    T& operator*() const noexcept
+    {
+        return *ptr;
+    }
+
+    T& operator[](std::size_t i) const noexcept
+    {
+        return ptr[i];
+    }
+
+    void Reset(T* newPtr = nullptr) noexcept
+    {
+        Deleter{}(ptr);
+        ptr = newPtr;
+    }
+
+    T* Release() noexcept
+    {
+        return std::exchange(ptr, nullptr);
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return ptr != nullptr;
     }
 
 private:
@@ -68,9 +170,22 @@ private:
 };
 
 template <class T, class... Args>
+    requires(not std::is_array_v<T>)
 constexpr UniqueHandle<T> MakeUnique(Args&&... args)
 {
     return UniqueHandle<T>(new T(std::forward<Args>(args)...));
 }
+
+template <class T>
+    requires(std::is_unbounded_array_v<T>)
+constexpr UniqueHandle<T> MakeUnique(std::size_t size)
+{
+    using ElementType = std::remove_extent_t<T>;
+    return UniqueHandle<T>(new ElementType[size]);
+}
+
+template <class T, class... Args>
+    requires(std::is_bounded_array_v<T>)
+constexpr UniqueHandle<T> MakeUnique(Args&&...) = delete;
 
 } // namespace vex
