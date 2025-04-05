e0e3b717becd5cef269ad729646b45b2e9047a75
Narvin Chana <narvin.chana@gmail.com>
2025-04-05 01:11:36 -0400

    revert: debug

--- src/Vulkan/VkRHI.cpp
+++ src/Vulkan/VkRHI.cpp
@@ -65,7 +65,7 @@ VkRHI::VkRHI(const PlatformWindowHandle& windowHandle, bool enableGPUDebugLayer,
         ::vk::DebugUtilsMessengerCreateInfoEXT debugCreateInfo{
             .messageSeverity = Severity::eVerbose | Severity::eWarning | Severity::eError,
             .messageType = MessageType::eGeneral | MessageType::eValidation | MessageType::ePerformance,
-            .pfnUserCallback = reinterpret_cast<::vk::PFN_DebugUtilsMessengerCallbackEXT>(debugCallback),
+            .pfnUserCallback = debugCallback,
         };
         debugCreateInfoPtr = &debugCreateInfo;
     }
