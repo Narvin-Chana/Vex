5c4f1461be794ce65eefcc7c779a2c647acaccb0
Narvin Chana <narvin.chana@gmail.com>
2025-07-03 20:51:41 -0400

    chore: linux

--- cmake/VexDXC.cmake
+++ cmake/VexDXC.cmake
@@ -31,6 +31,13 @@ function(build_with_dxc target)
         message(STATUS "Statically linked with DXC: ${DXC_STATIC_LIB}")
         target_link_libraries(${target} PRIVATE "${DXC_STATIC_LIB}")
     elseif(UNIX)
+        target_link_libraries(${target} PRIVATE "${DXC_SHARED_LIB}")
+        if(UNIX)
+            set_target_properties(${target} PROPERTIES
+                INSTALL_RPATH "${dxc_SOURCE_DIR}"
+                BUILD_WITH_INSTALL_RPATH TRUE
+            )
+        endif()
     endif()
     message(STATUS "Installing DXC headers: ${dxc_SOURCE_DIR}/${DXC_HEADERS_INCLUDE_NAME}")
     add_header_only_dependency(${target} dxc "${dxc_SOURCE_DIR}" "${DXC_HEADERS_INCLUDE_NAME}" "dxc")
