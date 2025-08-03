da7bebe417fbe23cb638916f5bbd95fc6c39b54e
Narvin Chana <narvin.chana@gmail.com>
2025-08-03 01:18:38 -0400

    feat: updated shader gen

--- src/Vex/ShaderGen.h
+++ src/Vex/ShaderGen.h
@@ -18,10 +18,12 @@ constexpr const char* ShaderGenBindingMacros = R"(
 
 // BINDING MACROS -------------------------
 
+// Usage: VEX_GLOBAL_RESOURCE(StructuredBuffer<Colors>, ColorBuffer);
+// Can now use ColorBuffer in your code as any other StructuredBuffer.
 #define VEX_GLOBAL_RESOURCE(type, name) static type name = ResourceDescriptorHeap[zzzZZZ___GeneratedConstantsCB.name##_bindlessIndex]
 
-// TODO(https://trello.com/c/bIVo8EP9): Users could also eventually get the bindless index (eg for storing a StructuredBuffer of materials, each material having bindless indices towards its textures such as albedo.)
-// In this case they should use this macro to get the resource, this macro should contain additional checks to avoid using invalid handles (should it really? validation layers have become really good at detecting invalid usage of descriptors...).
-#define VEX_GET_BINDLESS_RESOURCE(type, index) ResourceDescriptorHeap[index];
+// Usage: StructuredBuffer<MyStruct> myStruct = VEX_GET_BINDLESS_RESOURCE(index);
+// Can now use myStruct in your code as any other StructuredBuffer.
+#define VEX_GET_BINDLESS_RESOURCE(index) ResourceDescriptorHeap[index];
 
 )";
