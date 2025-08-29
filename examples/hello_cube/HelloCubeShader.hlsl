fe5aa5e2b09cd8d2e7149331d5ee53ddc546bb35
Narvin Chana <narvin.chana@gmail.com>
2025-08-29 16:46:40 -0400

    fix: added perspective matrix to hello cube shaders

--- examples/hello_cube/HelloCubeShader.hlsl
+++ examples/hello_cube/HelloCubeShader.hlsl
@@ -46,14 +46,28 @@ VSOutput VSMain(in float3 position : POSITION)
     float3x3 finalRotation = mul(rotationY, rotationX);
     float3 rotatedPosition = mul(finalRotation, scaledPosition);
     
-    float3 finalPosition = rotatedPosition + float3(-0.4f, 0.3f, 0);
+    float3 worldPosition = rotatedPosition + float3(-0.4f, 0.3f, -10);
     
-    vs.position = float4(finalPosition, 1);
+    // Quick scuffed projection matrix (perspective)
+    float fov = 1.57f; // ~90 degrees in radians
+    float aspect = 16.0f / 9.0f; // Assumes 16:9 aspect ratio
+    float near = 0.1f;
+    float far = 10000.0f;
+    
+    float f = 1.0f / tan(fov * 0.5f);
+    float4x4 projection = float4x4(
+        f / aspect, 0,  0,                           0,
+        0,          f,  0,                           0,
+        0,          0,  far / (near - far),         -1,
+        0,          0,  (near * far) / (near - far), 0
+    );
+    
+    vs.position = mul(projection, float4(worldPosition, 1));
 
     return vs;
 }
 
 float4 PSMain(VSOutput input) : SV_Target
 {
-    return abs(input.position.xyzw);
+    return float4(1, 1, 0, 1);
 }
