d848c1b7c7f000cfb1a448332fae249b8b2908a1
alexandreLBarrett <alexandre.lemarbre@gmail.com>
2025-10-06 22:47:01 -0400

    Update tests/SynchronisationTest.cpp
    Co-authored-by: Narvin Chana <36044215+Narvin-Chana@users.noreply.github.com>

--- tests/SynchronisationTest.cpp
+++ tests/SynchronisationTest.cpp
@@ -11,21 +11,6 @@
 namespace vex
 {
 
-static constexpr int fact(int n)
-{
-    if (n == 2)
-    {
-        return 2;
-    }
-    return n * fact(n - 1);
-}
-
-TEST(GraphicsTests, FactSampleTest)
-{
-    EXPECT_EQ(fact(2), 2);
-    EXPECT_EQ(fact(3), 6);
-}
-
 TEST(GraphicsTests, CreateGraphicsWithoutDebugLayers)
 {
     GfxBackend{ BackendDescription{
