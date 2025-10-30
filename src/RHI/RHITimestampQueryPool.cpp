4e6f0c013b5a92cb8ad75e019a70b5bba8ad496f
alex L-B <alexandre.lemarbre@gmail.com>
2025-10-29 22:50:00 -0400

    WIP first query pool draft

---
+++ src/RHI/RHITimestampQueryPool.cpp
@@ -0,0 +1,35 @@
+﻿#include "RHITimestampQueryPool.h"
+
+#include <Vex/GfxBackend.h>
+#include <Vex/Validation.h>
+
+namespace vex
+{
+
+RHITimestampQueryPoolBase::RHITimestampQueryPoolBase(GfxBackend& graphics)
+    : graphics{ graphics }
+{
+    timestampBuffer = graphics.CreateBuffer({ .name = "TimestampQueryReadback",
+                                              .usage = BufferUsage::GenericBuffer,
+                                              .byteSize = sizeof(u64) * MaxQuerySlotsAtOnce,
+                                              .memoryLocality = ResourceMemoryLocality::CPURead });
+}
+
+u64 RHITimestampQueryPoolBase::GetTimestampInterval(QueryHandle handle)
+{
+    VEX_CHECK(inFlightQueries.IsValid(handle), "QueryHandle needs to be valid to retrieve timestamp data");
+
+    MaybeUninitialized<Query> query = inFlightQueries.ExtractElement(handle);
+    u64 timestampBefore, timestampAfter;
+
+    VEX_CHECK(query->ready,
+              "Query needs to be ready to be read. This should be the case once the command list is closed")
+    {
+        ResourceMappedMemory mem = graphics->MapResource(timestampBuffer);
+        mem.ReadData(query->startIndex, std::as_writable_bytes(std::span{ &timestampBefore, 1 }));
+        mem.ReadData(query->endIndex, std::as_writable_bytes(std::span{ &timestampAfter, 1 }));
+    }
+
+    return timestampAfter - timestampBefore;
+}
+} // namespace vex
---
+++ src/RHI/RHITimestampQueryPool.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <Vex/RHIImpl/RHIBuffer.h>
+
+namespace vex
+{
+class GfxBackend;
+
+struct Query
+{
+    // indexes in timestamp queries buffer
+    uint32_t startIndex;
+    uint32_t endIndex;
+    bool ready;
+
+    // Gets closed in EndQuery
+    bool closed = false;
+};
+
+struct QueryHandle : Handle<QueryHandle>
+{
+};
+
+struct QueryReadbackContext
+{
+};
+
+class RHITimestampQueryPoolBase
+{
+    static constexpr u64 MaxQuerySlotsAtOnce = 10'000;
+
+    FreeList<Query, QueryHandle> inFlightQueries;
+    FreeListAllocator inflightQueriesIds;
+    Buffer timestampBuffer;
+    NonNullPtr<GfxBackend> graphics;
+
+public:
+    RHITimestampQueryPoolBase(GfxBackend& graphics);
+
+    // Records beginning of query
+    virtual QueryHandle BeginQuery() = 0;
+    // Records end of querry
+    virtual void EndQuery(QueryHandle handle) = 0;
+
+    // Called when commandlist is closed
+    // Makes timestamps available in the timestampBuffer at QueryHandle index for each query that were popped
+    virtual void ResolveTimestamps() = 0;
+
+    // Also invalidates QueryHandle
+    u64 GetTimestampInterval(QueryHandle handle);
+};
+
+} // namespace vex
