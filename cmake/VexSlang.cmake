717e1240e2b73b22df0a270f120008a65328de8d
Narvin Chana <narvin.chana@gmail.com>
2025-11-05 17:59:57 -0500

    fix: slang should now no longer be in a weird state when the project is configured for the first time

--- cmake/VexSlang.cmake
+++ cmake/VexSlang.cmake
@@ -1,49 +1,49 @@
 include(FetchContent)
 
-# Choose URLs based on platform
-if(WIN32)
-    set(SLANG_RELEASE_URL "https://github.com/shader-slang/slang/releases/download/v2025.14.3/slang-2025.14.3-windows-x86_64.zip")
-elseif(UNIX)
-    set(SLANG_RELEASE_URL "https://github.com/shader-slang/slang/releases/download/v2025.14.3/slang-2025.14.3-linux-x86_64.tar.gz")
-else()
-    message(FATAL_ERROR "Unsupported platform for Slang binaries")
+if (VEX_ENABLE_SLANG)
+    # Choose URLs based on platform
+    if(WIN32)
+        set(SLANG_RELEASE_URL "https://github.com/shader-slang/slang/releases/download/v2025.14.3/slang-2025.14.3-windows-x86_64.zip")
+    elseif(UNIX)
+        set(SLANG_RELEASE_URL "https://github.com/shader-slang/slang/releases/download/v2025.14.3/slang-2025.14.3-linux-x86_64.tar.gz")
+    else()
+        message(FATAL_ERROR "Unsupported platform for Slang binaries")
+    endif()
+
+    message(STATUS "Fetching slang...")
+    FetchContent_Declare(
+        slang
+        URL ${SLANG_RELEASE_URL}
+    )
+    FetchContent_MakeAvailable(slang)
+
+    # Create imported target for slang
+    set(SLANG_HEADERS_INCLUDE_NAME "include")
+    if(WIN32)
+        set(SLANG_STATIC_LIBS 
+            "${slang_SOURCE_DIR}/lib/gfx.lib"
+            "${slang_SOURCE_DIR}/lib/slang-rt.lib"
+            "${slang_SOURCE_DIR}/lib/slang.lib"
+        )
+        set(SLANG_SHARED_LIBS 
+            "${slang_SOURCE_DIR}/bin/gfx.dll"
+            "${slang_SOURCE_DIR}/bin/slang-glsl-module.dll"
+            "${slang_SOURCE_DIR}/bin/slang-glslang.dll"
+            "${slang_SOURCE_DIR}/bin/slang-llvm.dll"
+            "${slang_SOURCE_DIR}/bin/slang-rt.dll"
+            "${slang_SOURCE_DIR}/bin/slang.dll"
+        )
+    elseif(UNIX)
+        set(SLANG_SHARED_LIBS 
+            "${slang_SOURCE_DIR}/lib/libgfx.so"
+            "${slang_SOURCE_DIR}/lib/libslang-glsl-module.so"
+            "${slang_SOURCE_DIR}/lib/libslang-glslang.so"
+            "${slang_SOURCE_DIR}/lib/libslang-llvm.so"
+            "${slang_SOURCE_DIR}/lib/libslang-rt.so"
+            "${slang_SOURCE_DIR}/lib/libslang.so"
+        )
+    endif()
 endif()
-
-message(STATUS "Fetching slang...")
-FetchContent_Declare(
-    slang
-    URL ${SLANG_RELEASE_URL}
-)
-FetchContent_MakeAvailable(slang)
-
-# Create imported target for slang
-set(SLANG_HEADERS_INCLUDE_NAME "include")
-if(WIN32)
-    set(SLANG_STATIC_LIBS 
-        "${slang_SOURCE_DIR}/lib/gfx.lib"
-        "${slang_SOURCE_DIR}/lib/slang-rt.lib"
-        "${slang_SOURCE_DIR}/lib/slang.lib"
-    )
-    set(SLANG_SHARED_LIBS 
-        "${slang_SOURCE_DIR}/bin/gfx.dll"
-        "${slang_SOURCE_DIR}/bin/slang-glsl-module.dll"
-        "${slang_SOURCE_DIR}/bin/slang-glslang.dll"
-        "${slang_SOURCE_DIR}/bin/slang-llvm.dll"
-        "${slang_SOURCE_DIR}/bin/slang-rt.dll"
-        "${slang_SOURCE_DIR}/bin/slang.dll"
-    )
-elseif(UNIX)
-    set(SLANG_SHARED_LIBS 
-        "${slang_SOURCE_DIR}/lib/libgfx.so"
-        "${slang_SOURCE_DIR}/lib/libslang-glsl-module.so"
-        "${slang_SOURCE_DIR}/lib/libslang-glslang.so"
-        "${slang_SOURCE_DIR}/lib/libslang-llvm.so"
-        "${slang_SOURCE_DIR}/lib/libslang-rt.so"
-        "${slang_SOURCE_DIR}/lib/libslang.so"
-    )
-endif()
-
-set(VEX_ENABLE_SLANG OFF CACHE BOOL "Enables the possibility to use the Slang shader compiler backend.")
 
 function(build_with_slang target)
     if (VEX_ENABLE_SLANG)
