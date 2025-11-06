4ff4eb167ab360dd074c4779ca651f3cc8fe826e
Narvin Chana <narvin.chana@gmail.com>
2025-11-05 23:10:10 -0500

    feat: added tight alignment check to DX12FeatureChecker and refactored code to use D3DX12.h

--- src/DX12/DX12FeatureChecker.cpp
+++ src/DX12/DX12FeatureChecker.cpp
@@ -12,64 +12,42 @@
 namespace vex::dx12
 {
 
+static constexpr D3D_SHADER_MODEL GMinimumShaderModel = D3D_SHADER_MODEL_6_6;
+static constexpr D3D12_RAYTRACING_TIER GMinimumRayTracingTier = D3D12_RAYTRACING_TIER_1_0;
+static constexpr D3D12_MESH_SHADER_TIER GMinimumMeshShaderTier = D3D12_MESH_SHADER_TIER_1;
+static constexpr D3D_FEATURE_LEVEL GMinimumFeatureLevel = D3D_FEATURE_LEVEL_12_1;
+
 DX12FeatureChecker::DX12FeatureChecker(const ComPtr<ID3D12Device>& device)
     : device{ device }
 {
     // Try to get device5 interface (needed for ray tracing)
-    rayTracingSupported = false;
-    ComPtr<ID3D12Device5> device5;
-    if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&device5))))
+    chk << featureSupport.Init(device.Get());
+
+    // Vex requires a minimum feature level of 12_1.
+    if (featureSupport.MaxSupportedFeatureLevel() < GMinimumFeatureLevel)
     {
-        D3D12_FEATURE_DATA_D3D12_OPTIONS5 options5{};
-        if (SUCCEEDED(device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS5, &options5, sizeof(options5))))
-        {
-            rayTracingSupported = options5.RaytracingTier >= MinimumRayTracingTier;
-        }
+        VEX_LOG(Fatal,
+                "DX12RHI incompatible: Vex DX12RHI requires feature level 12_1 which is not supported by your GPU.");
     }
 
-    // Initialize feature level check data
-    // Vex requires a minimum feature level of 12_1.
-    static const D3D_FEATURE_LEVEL d3dFeatureLevels[] = { D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1 };
-    featureLevels.NumFeatureLevels = _countof(d3dFeatureLevels);
-    featureLevels.pFeatureLevelsRequested = d3dFeatureLevels;
-
-    chk << device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS, &options, sizeof(options));
-    chk << device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS1, &options1, sizeof(options1));
-    chk << device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS7, &options7, sizeof(options7));
-    chk << device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS12, &options12, sizeof(options12));
-
     // Vex requires DX12's EnhancedBarriers for GPU resource synchronization.
-    if (!options12.EnhancedBarriersSupported)
+    if (!featureSupport.EnhancedBarriersSupported())
     {
         VEX_LOG(Fatal, "DX12RHI incompatible: Vex DX12RHI uses Enhanced Barriers which are not supported by your GPU.");
     }
 
-    chk << device->CheckFeatureSupport(D3D12_FEATURE_FEATURE_LEVELS, &featureLevels, sizeof(featureLevels));
-    // List of shader models to check, from highest to lowest
-    static const D3D_SHADER_MODEL shaderModelsToCheck[] = {
-        D3D_SHADER_MODEL_6_9, D3D_SHADER_MODEL_6_8, D3D_SHADER_MODEL_6_7, D3D_SHADER_MODEL_6_6, D3D_SHADER_MODEL_6_5,
-        D3D_SHADER_MODEL_6_4, D3D_SHADER_MODEL_6_3, D3D_SHADER_MODEL_6_2, D3D_SHADER_MODEL_6_1, D3D_SHADER_MODEL_6_0
-    };
-
-    // Find the highest supported shader model
-    bool shaderModelFound = false;
-    for (const auto& model : shaderModelsToCheck)
+    // Vex requires DX12's TightAlignment for tighter alignment.
+    if (featureSupport.TightAlignmentSupportTier() < D3D12_TIGHT_ALIGNMENT_TIER_NOT_SUPPORTED)
     {
-        shaderModel.HighestShaderModel = model;
-        if (SUCCEEDED(device->CheckFeatureSupport(D3D12_FEATURE_SHADER_MODEL, &shaderModel, sizeof(shaderModel))))
-        {
-            shaderModelFound = true;
-            break;
-        }
+        VEX_LOG(Fatal, "DX12RHI incompatible: Vex DX12RHI uses Tight Alignment which is not supported by your GPU.");
     }
 
-    // For correctness, RT requires SM_6_3+.
-    rayTracingSupported &= shaderModel.HighestShaderModel >= D3D_SHADER_MODEL_6_3;
-
-    if (!shaderModelFound || shaderModel.HighestShaderModel < D3D_SHADER_MODEL_6_6)
+    // Vex requires SM6_6 for bindless (currently a hard requirement due to Vex not supporting "bindful" code).
+    if (featureSupport.HighestShaderModel() < GMinimumShaderModel)
     {
         VEX_LOG(Fatal,
-                "Vex's DX12 implementation requires at least SM_6_6 for the untyped ResourceDescriptorHeap feature.");
+                "DX12RHI incompatible: Vex's DX12 implementation requires at least SM_6_6 for the untyped "
+                "ResourceDescriptorHeap feature.");
     }
 }
 
@@ -81,32 +59,38 @@ bool DX12FeatureChecker::IsFeatureSupported(Feature feature) const
     {
     case Feature::MeshShader:
         // Check mesh shader support through options7
-        return options7.MeshShaderTier >= MinimumMeshShaderTier;
+        return featureSupport.MeshShaderTier() >= GMinimumMeshShaderTier;
     case Feature::RayTracing:
+    {
+        bool rayTracingSupported = featureSupport.RaytracingTier() >= GMinimumRayTracingTier;
+        // For correctness, RT also requires SM_6_3+.
+        rayTracingSupported &= featureSupport.HighestShaderModel() >= D3D_SHADER_MODEL_6_3;
         return rayTracingSupported;
+    }
     case Feature::BindlessResources:
-        return shaderModel.HighestShaderModel >= D3D_SHADER_MODEL_6_6;
+        return featureSupport.HighestShaderModel() >= GMinimumShaderModel;
     case Feature::MipGeneration:
         // DX12 has no built-in way to generate mip-maps.
         return false;
     default:
+        VEX_LOG(Fatal, "Unable to determine feature support for {}", magic_enum::enum_name(feature));
         return false;
     }
 }
 
 FeatureLevel DX12FeatureChecker::GetFeatureLevel() const
 {
-    return ConvertDX12FeatureLevelToFeatureLevel(featureLevels.MaxSupportedFeatureLevel);
+    return ConvertDX12FeatureLevelToFeatureLevel(featureSupport.MaxSupportedFeatureLevel());
 }
 
 ResourceBindingTier DX12FeatureChecker::GetResourceBindingTier() const
 {
-    return ConvertDX12ResourceBindingTierToResourceBindingTier(options.ResourceBindingTier);
+    return ConvertDX12ResourceBindingTierToResourceBindingTier(featureSupport.ResourceBindingTier());
 }
 
 ShaderModel DX12FeatureChecker::GetShaderModel() const
 {
-    return ConvertDX12ShaderModelToShaderModel(shaderModel.HighestShaderModel);
+    return ConvertDX12ShaderModelToShaderModel(featureSupport.HighestShaderModel());
 }
 
 u32 DX12FeatureChecker::GetMaxLocalConstantsByteSize() const
@@ -173,28 +157,29 @@ ResourceBindingTier DX12FeatureChecker::ConvertDX12ResourceBindingTierToResource
 
 ShaderModel DX12FeatureChecker::ConvertDX12ShaderModelToShaderModel(D3D_SHADER_MODEL shaderModel)
 {
+    using enum ShaderModel;
     switch (shaderModel)
     {
     case D3D_SHADER_MODEL_6_0:
-        return ShaderModel::SM_6_0;
+        return SM_6_0;
     case D3D_SHADER_MODEL_6_1:
-        return ShaderModel::SM_6_1;
+        return SM_6_1;
     case D3D_SHADER_MODEL_6_2:
-        return ShaderModel::SM_6_2;
+        return SM_6_2;
     case D3D_SHADER_MODEL_6_3:
-        return ShaderModel::SM_6_3;
+        return SM_6_3;
     case D3D_SHADER_MODEL_6_4:
-        return ShaderModel::SM_6_4;
+        return SM_6_4;
     case D3D_SHADER_MODEL_6_5:
-        return ShaderModel::SM_6_5;
+        return SM_6_5;
     case D3D_SHADER_MODEL_6_6:
-        return ShaderModel::SM_6_6;
+        return SM_6_6;
     case D3D_SHADER_MODEL_6_7:
-        return ShaderModel::SM_6_7;
+        return SM_6_7;
     case D3D_SHADER_MODEL_6_8:
-        return ShaderModel::SM_6_8;
+        return SM_6_8;
     case D3D_SHADER_MODEL_6_9:
-        return ShaderModel::SM_6_9;
+        return SM_6_9;
     default:
         VEX_LOG(Fatal, "Unsupported shader model: {}.", magic_enum::enum_name(shaderModel));
     }
--- src/DX12/DX12FeatureChecker.h
+++ src/DX12/DX12FeatureChecker.h
@@ -30,15 +30,6 @@ private:
     ComPtr<ID3D12Device> device;
 
     // Cached feature support data (to avoid requerying the device).
-    D3D12_FEATURE_DATA_D3D12_OPTIONS options;
-    D3D12_FEATURE_DATA_D3D12_OPTIONS1 options1;
-    D3D12_FEATURE_DATA_D3D12_OPTIONS7 options7;
-    D3D12_FEATURE_DATA_D3D12_OPTIONS12 options12;
-    D3D12_FEATURE_DATA_SHADER_MODEL shaderModel;
-    D3D12_FEATURE_DATA_FEATURE_LEVELS featureLevels;
-    bool rayTracingSupported = false;
-
-    static constexpr D3D12_RAYTRACING_TIER MinimumRayTracingTier = D3D12_RAYTRACING_TIER_1_0;
-    static constexpr D3D12_MESH_SHADER_TIER MinimumMeshShaderTier = D3D12_MESH_SHADER_TIER_1;
+    CD3DX12FeatureSupport featureSupport;
 };
 } // namespace vex::dx12
