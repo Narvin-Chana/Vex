78db235984e05c46ec5ee9e9d0c79340e0dd815e
md <martin.dufour@havenstudios.com>
2026-09-16 10:52:52 -0400

    Refactor embedded shader header to use std::string_view and fix output directory structure

--- cmake/VexEmbedShaders.cmake
+++ cmake/VexEmbedShaders.cmake
@@ -13,26 +13,28 @@ if (VEX_USE_EMBEDDED_SHADERS)
     # Re-configure when any shader source changes
     set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${_vex_shader_deps})
 
-    set(_vex_embedded_header "#pragma once\n\nnamespace vex::embedded\n{\n\n")
+    set(_vex_embedded_header "#pragma once\n\n#include <string_view>\n\nnamespace vex::embedded\n{\n\n")
 
     if (VEX_ENABLE_DXC)
         file(READ "${VEX_ROOT_DIR}/shaders/Vex.hlsli" _vex_hlsl_src)
         string(APPEND _vex_embedded_header
-            "inline constexpr const char* VexHLSL = R\"VEX_SHADER_DELIM(\n${_vex_hlsl_src}\n)VEX_SHADER_DELIM\";\n\n")
+            "inline constexpr const std::string_view VexHLSL = R\"VEX_SHADER_DELIM(\n${_vex_hlsl_src}\n)VEX_SHADER_DELIM\";\n\n")
     endif()
 
     if (VEX_ENABLE_SLANG)
         file(READ "${VEX_ROOT_DIR}/shaders/Vex.slang" _vex_slang_src)
         string(APPEND _vex_embedded_header
-            "inline constexpr const char* VexSlang = R\"VEX_SHADER_DELIM(\n${_vex_slang_src}\n)VEX_SHADER_DELIM\";\n\n")
+            "inline constexpr const std::string_view VexSlang = R\"VEX_SHADER_DELIM(\n${_vex_slang_src}\n)VEX_SHADER_DELIM\";\n\n")
     endif()
 
     string(APPEND _vex_embedded_header "} // namespace vex::embedded\n")
 
-    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/src/Vex/Generated")
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/src/Vex/Generated/EmbeddedShaders.h" "${_vex_embedded_header}")
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gen/ShaderCompiler/Generated")
+    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/gen/ShaderCompiler/Generated/EmbeddedShaders.h" "${_vex_embedded_header}")
 
-    target_include_directories(Vex PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/src")
+    target_include_directories(Vex PUBLIC 
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gen/ShaderCompiler/Generated>"
+    $<INSTALL_INTERFACE:include/vex/ShaderCompiler/Generated>)
 
     target_compile_definitions(Vex PRIVATE VEX_EMBEDDED_SHADERS=1)
 else()
